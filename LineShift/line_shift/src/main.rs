a717b4397dfaaf279a37fffdf34a24972e9fa9ee
Illia Khurtin <ikhurtin@wfsaustralia.com>
2019-11-06 14:26:08 +1100

    Using u256 as a vector.

--- LineShift/line_shift/src/main.rs
+++ LineShift/line_shift/src/main.rs
@@ -1,24 +1,24 @@
 extern crate bigint;
-extern crate bit_vec;
 
 use bigint::U256;
-use bit_vec::BitVec;
-use std::mem;
+use std::u16;
 
 fn main() {
-	let initial_vec = create_bitmap_line();
-	println!();
+	let initial_vec = create_bitmap();
+	println!("Initial vector: ");
 	print_bitmap(&initial_vec);
 
-	let shifted_vec = shift_bitmap_line(&initial_vec, 1);
-	println!();
-	print_bitmap(&shifted_vec);
+	let u256_vec = convert_to_u256(&initial_vec);
+	println!("Vector as U256: ");
+	print_u256_in_rows(&u256_vec);
 
-	convert_to_bit_vec(&initial_vec);
+	let shifted_vec = shift_bitmap(&initial_vec, 1);
+	println!("Shifted vector: ");
+	print_bitmap(&shifted_vec);
 }
 
 // creates bitmap 16x16 with a line of 1s inside
-fn create_bitmap_line() -> Vec<u16> {
+fn create_bitmap() -> Vec<u16> {
 	let vec = vec![
 		0b_0000_0000_0000_0000,
 		0b_0000_0110_0000_0000,
@@ -40,7 +40,7 @@ fn create_bitmap_line() -> Vec<u16> {
 	vec
 }
 
-fn shift_bitmap_line(vec: &Vec<u16>, shift: u16) -> Vec<u16> {
+fn shift_bitmap(vec: &Vec<u16>, shift: u16) -> Vec<u16> {
 	let mut result = Vec::with_capacity(vec.len());
 	for row in vec {
 		let new_row = row >> shift;
@@ -52,7 +52,7 @@ fn shift_bitmap_line(vec: &Vec<u16>, shift: u16) -> Vec<u16> {
 fn print_bitmap(vec: &Vec<u16>) {
 	for row in vec {
 		for bit_idx in 0..16 {
-			let mut bit = (row << bit_idx) & 0b_1000_0000_0000_0000_u16;
+			let mut bit = (row >> bit_idx) & 0b_0000_0000_0000_0001_u16;
 			if bit != 0 {
 				bit = 1u16;
 			}
@@ -62,27 +62,44 @@ fn print_bitmap(vec: &Vec<u16>) {
 	}
 }
 
-fn print_bit_vec(bit_vec: &BitVec) {
-	for bit in bit_vec {
-		print!("{}", bit as u8);
+fn print_u256(vec: &U256) {
+	for i in 0..256 {
+		let bit = vec.bit(i);
+		if bit {
+			print!("1 ");
+		} else {
+			print!("0 ");
+		}
 	}
+	println!("")
 }
 
-fn convert_to_bytes(vec: &Vec<u16>) -> Vec<u8> {
-	let capacity = 2 * vec.len();
-	let mut byte_vec = Vec::with_capacity(capacity);
+fn print_u256_in_rows(vec: &U256) {
+	let row_max_idx = 16;
+	let col_max_idx = 16;
+	for i in 0..row_max_idx {
+		for j in 0..col_max_idx {
+			let idx = i * col_max_idx + j;
+			let bit = vec.bit(idx);
+			if bit {
+				print!("1 ");
+			} else {
+				print!("0 ");
+			}
+		}
+		println!("");
+	}
+	println!("")
+}
+
+fn convert_to_u256(vec: &Vec<u16>) -> U256 {
+	let mut result = U256::from(0x00);
+	let mut idx = 0;
 	for row in vec {
-		let bytes = row.to_be_bytes();
-		byte_vec.push(bytes[0]);
-		byte_vec.push(bytes[1]);
+		let mut item = U256::from(*row);
+		item = item << 16 * idx;
+		result = result | item;
+		idx = idx + 1;
 	}
-	byte_vec
-}
-
-fn convert_to_bit_vec(vec: &Vec<u16>) {
-	let byte_vec = convert_to_bytes(&vec);
-	let mut bit_vec = BitVec::from_bytes(&byte_vec);
-	print_bit_vec(&bit_vec);
-	println!();
-	println!("bit vec size: {}", bit_vec.len());
+	result
 }
