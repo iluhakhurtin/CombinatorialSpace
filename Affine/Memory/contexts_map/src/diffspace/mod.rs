2f576523af3c91db0091669412736a3ade365423
Illia Khurtin <iluhakhurtin@gmail.com>
2019-12-03 09:14:39 +1100

    Covariances distances calculation

--- Affine/Memory/contexts_map/src/diffspace/mod.rs
+++ Affine/Memory/contexts_map/src/diffspace/mod.rs
@@ -8,8 +8,11 @@ use context_map::ContextMap;
 use ndarray::Array2;
 
 pub fn learn(contexts: &mut ContextMap, code: &BitVector) {
-	//get covariances for the contexts
+	// get covariances for the contexts
 	let covariances = calculate_covariances_map(&contexts, &code);
+
+    // get contexts' covariances distances map with total distance
+    let (total_distance, distances) = calculate_contexts_covariances_distances(&covariances);
 
 	// for ((x, y), cov) in covariances.indexed_iter() {
 	// 	println!("x: {0}, y: {1}, cov: {2}", x, y, cov);
@@ -33,6 +36,29 @@ fn calculate_covariances_map(contexts: &ContextMap, code: &BitVector) -> Array2<
 	covariances
 }
 
-fn build_contexts_distances(covariances: Array2<f32>) -> (f32, Vec<(usize, usize)>) {
-	(0., vec![])
+/// Builds condexts distances map in which every context's coordinates
+/// are placed alongside a line in which the distances are represented
+/// as the gaps between numbers. The more covariance is the bigger gap
+/// the contexts occupies on the line. 
+/// The idea is to increase the chnances to be randomly selected for a 
+/// context which has higher covariance:
+/// 1 1 1 10 100 10 1 1 1 1
+/// 1 2 3 13 113 123 124 125.
+/// The method returns total distance (the length of the line) and a 
+/// vector which links coordinates of a context and the distance.
+fn calculate_contexts_covariances_distances(covariances: &Array2<f32>) -> (f32, Vec<((usize, usize), f32)>) {
+    // Total distance to be used in weighted random selection.
+    let mut total_distance: f32 = 0.;
+
+    // Selection array. Each candidate is placed along the imaginary line,
+    // occupying a line segment propoptional to context's covariance.
+    let contexts_distances: Vec<_> = covariances
+        .indexed_iter()
+        .map(|(coords, covariance)| {
+            total_distance += covariance;
+            (coords, total_distance)
+        })
+        .collect();
+
+	(total_distance, contexts_distances)
 }
