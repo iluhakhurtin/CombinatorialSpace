0ed9045a46320801616317bb35dc9fcc41add85a
Illia Khurtin <ikhurtin@wfsaustralia.com>
2019-12-03 17:28:53 +1100

    Initial variation. Getting contexts to learn.

--- Affine/Memory/contexts_map/src/diffspace/mod.rs
+++ Affine/Memory/contexts_map/src/diffspace/mod.rs
@@ -5,19 +5,35 @@ mod context;
 pub mod context_map;
 use bitvector::BitVector;
 use context_map::ContextMap;
-use ndarray::Array2;
 use float_cmp::ApproxOrdUlps;
+use ndarray::Array2;
+use rand::Rng;
 
 pub fn learn(contexts: &mut ContextMap, code: &BitVector) {
 	// get covariances for the contexts
 	let covariances = calculate_covariances_map(&contexts, &code);
 
-    // get contexts' covariances distances map with total distance
-    let (total_distance, distances) = calculate_contexts_covariances_distances(&covariances);
+	// get contexts' covariances distances map with total distance
+	let (total_distance, distances) = calculate_contexts_covariances_distances(&covariances);
 
-	// for ((x, y), cov) in covariances.indexed_iter() {
-	// 	println!("x: {0}, y: {1}, cov: {2}", x, y, cov);
-	// }
+	// winner context's coordinates
+	let candidate_coordinates = get_winner_coordinates(&total_distance, &distances);
+
+	// get contexts map dimentions
+	let learn_range = get_learn_range();
+
+	let shape = contexts.shape();
+	let max_dim_y = shape[0];
+	let start_y = candidate_coordinates.0 as isize - learn_range;
+	let end_y = candidate_coordinates.0 as isize + learn_range;
+	let range_y = clamp_index(&start_y, &max_dim_y)..=clamp_index(&end_y, &max_dim_y);
+
+	let max_dim_x = shape[1];
+	let start_x = candidate_coordinates.1 as isize - learn_range;
+	let end_x = candidate_coordinates.1 as isize + learn_range;
+	let range_x = clamp_index(&start_x, &max_dim_x)..=clamp_index(&end_x, &max_dim_x);
+
+	let mut contexts_to_learn = contexts.slice_mut(s![range_y, range_x]);
 }
 
 fn calculate_covariances_map(contexts: &ContextMap, code: &BitVector) -> Array2<f32> {
@@ -32,6 +48,10 @@ fn calculate_covariances_map(contexts: &ContextMap, code: &BitVector) -> Array2<
 			})
 			.sum();
 
+		if covariance == 0f32 {
+			let result = get_min_covariance();
+			return result;
+		}
 		covariance
 	});
 	covariances
@@ -40,45 +60,67 @@ fn calculate_covariances_map(contexts: &ContextMap, code: &BitVector) -> Array2<
 /// Builds condexts distances map in which every context's coordinates
 /// are placed alongside a line in which the distances are represented
 /// as the gaps between numbers. The more covariance is the bigger gap
-/// the contexts occupies on the line. 
-/// The idea is to increase the chnances to be randomly selected for a 
+/// the contexts occupies on the line.
+/// The idea is to increase the chnances to be randomly selected for a
 /// context which has higher covariance:
 /// 1 1 1 10 100 10 1 1 1 1
 /// 1 2 3 13 113 123 124 125.
-/// The method returns total distance (the length of the line) and a 
+/// The method returns total distance (the length of the line) and a
 /// vector which links coordinates of a context and the distance.
-fn calculate_contexts_covariances_distances(covariances: &Array2<f32>) -> (f32, Vec<((usize, usize), f32)>) {
-    // Total distance to be used in weighted random selection.
-    let mut total_distance: f32 = 0.;
+fn calculate_contexts_covariances_distances(
+	covariances: &Array2<f32>,
+) -> (f32, Vec<((usize, usize), f32)>) {
+	// Total distance to be used in weighted random selection.
+	let mut total_distance: f32 = 0.;
 
-    // Selection array. Each candidate is placed along the imaginary line,
-    // occupying a line segment propoptional to context's covariance.
-    let contexts_distances: Vec<_> = covariances
-        .indexed_iter()
-        .map(|(coords, covariance)| {
-            total_distance += covariance;
-            (coords, total_distance)
-        })
-        .collect();
+	// Selection array. Each candidate is placed along the imaginary line,
+	// occupying a line segment propoptional to context's covariance.
+	let contexts_distances: Vec<_> = covariances
+		.indexed_iter()
+		.map(|(coords, covariance)| {
+			total_distance += covariance;
+			(coords, total_distance)
+		})
+		.collect();
 
 	(total_distance, contexts_distances)
 }
 
-fn get_candidate_coordinates(distances: &Vec<((usize, usize), f32)>, total_distance: &f32) -> (usize, usize) {
-    let mut rng = rand::thread_rng();
+fn get_winner_coordinates(
+	total_distance: &f32,
+	distances: &Vec<((usize, usize), f32)>,
+) -> (usize, usize) {
+	let mut rng = rand::thread_rng();
 
-    // Random value to be used for candidate selection.
-    // The larger the covariance, the more chances are that
-    // this context will be selected by the binary search.
-    let pick = rng.gen_range(0., total_distance);
+	// Random value to be used for candidate selection.
+	// The larger the covariance, the more chances are that
+	// this context will be selected by the binary search.
+	let pick = rng.gen_range(0., total_distance);
 
-    // Find an index of a segment that encloses random value.
-    let target_index = match distances.binary_search_by(|(_, distance)| distance.approx_cmp_ulps(&pick, 2)) {
-        Ok(index) => index,
-        Err(index) => index,
-    };
+	// Find an index of a segment that encloses random value.
+	let target_index =
+		match distances.binary_search_by(|(_, distance)| distance.approx_cmp_ulps(&pick, 2)) {
+			Ok(index) => index,
+			Err(index) => index,
+		};
 
+	let target_coordinates = distances[target_index].0;
 
+	target_coordinates
+}
 
-    (1, 1)
+fn clamp_index(index: &isize, max_dim: &usize) -> usize {
+	(match index {
+		_ if *index < 0 => 0,
+		_ if *index > (max_dim - 1) as isize => max_dim - 1,
+		_ => *index as usize,
+	})
+}
+
+fn get_min_covariance() -> f32 {
+	0.0001
+}
+
+fn get_learn_range() -> isize {
+	4
 }
