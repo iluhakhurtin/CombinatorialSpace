3acd74587d9f200f4d240068dd881540ee694af8
Illia Khurtin <ikhurtin@wfsaustralia.com>
2019-12-13 16:34:31 +1100

    Printing in image from a starting point.

--- Affine/Memory/contexts_map/src/main.rs
+++ Affine/Memory/contexts_map/src/main.rs
@@ -9,7 +9,7 @@ use rand::Rng;
 
 fn main() {
 	let codes = diffspace::code_space::generate_code_space();
-	// draw_codes(&codes);
+	// draw_codes_space(&codes);
 
 	const CONTEXT_MAP_MAX_DIM: usize = 64;
 	let mut contexts = diffspace::context_map::generate_context_map(CONTEXT_MAP_MAX_DIM);
@@ -36,17 +36,29 @@ fn main() {
 
 		// print every n-th step
 		if step % 10000 > 0 {
-			let file_name = format!("r_{}.png", step);
-			draw_contexts_activation_map(&contexts, &test_code_r, &file_name);
-			let file_name = format!("g_{}.png", step);
-			draw_contexts_activation_map(&contexts, &test_code_g, &file_name);
-			let file_name = format!("b_{}.png", step);
-			draw_contexts_activation_map(&contexts, &test_code_b, &file_name);
+			let shape = contexts.shape();
+			let height = shape[0] as u32 + 2; //2 is for printing the code on the top
+			let width = 128;
+			let mut start_x = 0;
+			let mut start_y = 0;
+
+			//type GrayImage = ImageBuffer<Luma<u8>, Vec<u8>>;
+			let mut image = image::GrayImage::new(width, height);
+			draw_contexts_activation_map(&contexts, &test_code_r, &mut image, start_x, start_y);
+
+			// let file_name = format!("g_{}.png", step);
+			// draw_contexts_activation_map(&contexts, &test_code_g, &image);
+			// let file_name = format!("b_{}.png", step);
+			// draw_contexts_activation_map(&contexts, &test_code_b, &image);
+			let file_name = format!("{}.png", step);
+			image
+				.save(format!("output/activation_maps/{}", file_name))
+				.unwrap();
 		}
 	}
 }
 
-fn draw_codes(codes: &CodeSpace) {
+fn draw_codes_space(codes: &CodeSpace) {
 	let width = 128;
 	let height = codes.len() as u32;
 	let mut image = image::GrayImage::new(width, height);
@@ -65,14 +77,13 @@ fn draw_codes(codes: &CodeSpace) {
 	image.save("output/code_space.png").unwrap();
 }
 
-fn draw_contexts_activation_map(contexts: &ContextMap, code: &BitVector, file_name: &String) {
-	let shape = contexts.shape();
-	let height = (shape[0] + 2) as u32; //2 is for printing the code on the top
-	let width = 128;
-
-	//type GrayImage = ImageBuffer<Luma<u8>, Vec<u8>>;
-	let mut image = image::GrayImage::new(width, height);
-
+fn draw_contexts_activation_map(
+	contexts: &ContextMap,
+	code: &BitVector,
+	image: &mut image::GrayImage,
+	start_x: u32,
+	start_y: u32,
+) {
 	// 1. Print contexts activation map
 	for ((y, x), context) in contexts.indexed_iter() {
 		let covariance_round = 5. * context.covariance(code).round();
@@ -84,26 +95,26 @@ fn draw_contexts_activation_map(contexts: &ContextMap, code: &BitVector, file_na
 		};
 
 		let pixel: image::Luma<u8> = image::Luma([brightness]);
-		image.put_pixel(x as u32, y as u32, pixel);
+		let new_x = start_x + x as u32;
+		let new_y = start_y + y as u32;
+		image.put_pixel(new_x, new_y, pixel);
 	}
 
 	// 2. Print separator
+	let width = 128;
+	let shape = contexts.shape();
 	let pixel: image::Luma<u8> = image::Luma([128]);
-	let y = height - 2;
+	let mut y = shape[0] as u32;
 	for x in 0..width {
 		image.put_pixel(x, y, pixel);
 	}
 
 	// 3. Print the code: white is 1
-	let y = height - 1;
-	for x in 0..128 {
+	y += 1;
+	for x in 0..width {
 		let brightness = if code[x as usize] { 255 } else { 0 };
 
 		let pixel: image::Luma<u8> = image::Luma([brightness]);
 		image.put_pixel(x, y, pixel);
 	}
-
-	image
-		.save(format!("output/activation_maps/{}", file_name))
-		.unwrap();
 }
